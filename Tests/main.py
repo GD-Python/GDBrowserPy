ed3a105491326eca5cdbe26706b4cc6c8a65610e
mel0n7 <99061680+Mel0n7@users.noreply.github.com>
2022-02-13 17:57:22 +0000

    v0.0.1

--- Tests/main.py
+++ Tests/main.py
@@ -1,2 +1,20 @@
 from GDBrowserPy import GD
-print(GD.Level(128).name)
+level = GD.Level(128)
+level.name #Name of Level
+level.id #ID of Level
+level.description #Description of Level
+level.author #Author of Level
+level.playerID #Player ID of Author
+level.accountID #Account ID of Author
+level.difficulty #Difficulty of Level
+level.downloads #Downloads of Level
+level.likes #Likes of Level
+level.disliked #Disliked Level
+level.length #Length of Level
+level.stars #Stars of Level
+level.orbs #Orbs of Level
+level.diamonds #Diamonds of Level
+level.featured #Is Level featured (true or false)
+level.name #Name of Level
+level.name #Name of Level
+level.name #Name of Level
--- install.py
+++
@@ -1,7 +0,0 @@
-import shutil, site, os
-folder = site.getsitepackages()[0]
-os.chdir(folder)
-os.mkdir(os.getcwd() + "GDBrowserPy")
-files = ['/home/runner/GDBrowserPy/GDBrowserPy/__init__.py']
-for f in files:
-    shutil.copy(f, 'GDBrowserPy')
